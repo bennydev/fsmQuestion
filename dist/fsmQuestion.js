7655bac1988175accfba9473360d236cbed4a920
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2015-12-07 15:17:47 +0100

    Renamed js files to get correct ordering.

--- dist/fsmQuestion.js
+++ dist/fsmQuestion.js
@@ -1,3 +1,6 @@
+"use strict";
+angular.module('fsmQuestion', []);
+
 'use strict';
 angular.module('fsmQuestion').factory('ErrorAdapter', ['ErrorReporter', function (ErrorReporter) {
     var errorAdapter = {
@@ -88,6 +91,87 @@ angular.module('fsmQuestion').factory('ErrorReporter', ['$log', function($log){
     };
 }]);
 
+'use strict';
+
+angular.module('fsmQuestion')
+    .directive('fsmQuestion',['QuestionService', 'ErrorReporter', 'FileUploaderService', function(QuestionService, ErrorReporter, FileUploaderService){
+        function init(scope){
+            initScopeVariables(scope);
+            initQuestionModel(scope);
+            if(!scope.question.isAnswered() || scope.question.getAnswer() === QuestionService.getOptions().select.value) {
+                scope.question.loadAnswer();
+            }
+        }
+
+        function initScopeVariables(scope){
+            scope.types = QuestionService.getTypes();
+            scope.errors = ErrorReporter.errors;
+        }
+
+        function initQuestionModel(scope){
+            var question = scope.question;
+            if(question.type === scope.types.select){
+                if(!question.getAnswer()){
+                    question.model[question.id] = question.options[0].value;
+                }
+            }
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                question: '=',
+                translateValues: '='
+
+            },
+            templateUrl: 'fsmQuestion/templates/question.tpl.html',
+            link: function(scope, element, attributes){
+                init(scope);
+                //UPLOAD
+                if(scope.question.type === QuestionService.getTypes().upload){
+                    var isVisible = scope.question.isVisible;
+                    scope.question.isVisible = function(){return FileUploaderService.fileUploadSupported() && isVisible();};
+                }
+
+                //PHONE
+                if(scope.question.type === QuestionService.getTypes().phone){
+                    var onChange = scope.question.onChange ? scope.question.onChange : function(){};
+                    scope.question.onChange = function(question){
+                        if(question.model[question.id].countryCode.code === 'SWE') {
+                            if (question.model[question.id].phoneNumber === '') {
+                                question.model[question.id].phoneNumber = '0';
+                            }
+
+                            if(question.getAnswer().phoneNumber.toString().length >= 2 && question.getAnswer().phoneNumber.toString().substr(0,2) === '00'){
+                                question.model[question.id].phoneNumber = '0'+question.getAnswer().phoneNumber.toString().substr(2);
+                            }
+                        }
+                        onChange(question);
+                    };
+                    scope.addZeroToPhonenumber = function(){
+                        if(scope.question.model[scope.question.id].phoneNumber === '0' && scope.question.model[scope.question.id].countryCode.code !== 'SWE'){
+                            scope.question.model[scope.question.id].phoneNumber = '';
+                        } else if(scope.question.model[scope.question.id].phoneNumber === '' && scope.question.model[scope.question.id].countryCode.code === 'SWE'){
+                            scope.question.model[scope.question.id].phoneNumber = '0';
+                        }
+                    };
+                }
+            }
+        };
+    }]);
+
+'use strict';
+angular.module('fsmQuestion').filter('fsmTranslate', ['$translate', function($translate){
+    function fsmTranslate(key){
+        var translation = $translate.instant(key);
+        if(translation === key){
+            return '';
+        }
+        return translation;
+    }
+
+    return fsmTranslate;
+}]);
 'use strict';
 angular.module('fsmQuestion').factory('QuestionService', ['ValidationService', 'localStorageService', 'ErrorAdapter', '$filter', 'ClaimService',
     function (ValidationService, localStorageService, ErrorAdapter, $filter, ClaimService) {
@@ -691,90 +775,6 @@ angular.module('fsmQuestion').factory('ValidationService', ['ErrorReporter', fun
     };
 }]);
 
-"use strict";
-angular.module('fsmQuestion', []);
-
-'use strict';
-
-angular.module('fsmQuestion')
-    .directive('fsmQuestion',['QuestionService', 'ErrorReporter', 'FileUploaderService', function(QuestionService, ErrorReporter, FileUploaderService){
-        function init(scope){
-            initScopeVariables(scope);
-            initQuestionModel(scope);
-            if(!scope.question.isAnswered() || scope.question.getAnswer() === QuestionService.getOptions().select.value) {
-                scope.question.loadAnswer();
-            }
-        }
-
-        function initScopeVariables(scope){
-            scope.types = QuestionService.getTypes();
-            scope.errors = ErrorReporter.errors;
-        }
-
-        function initQuestionModel(scope){
-            var question = scope.question;
-            if(question.type === scope.types.select){
-                if(!question.getAnswer()){
-                    question.model[question.id] = question.options[0].value;
-                }
-            }
-        }
-
-        return {
-            restrict: 'E',
-            scope: {
-                question: '=',
-                translateValues: '='
-
-            },
-            templateUrl: 'fsmQuestion/templates/question.tpl.html',
-            link: function(scope, element, attributes){
-                init(scope);
-                //UPLOAD
-                if(scope.question.type === QuestionService.getTypes().upload){
-                    var isVisible = scope.question.isVisible;
-                    scope.question.isVisible = function(){return FileUploaderService.fileUploadSupported() && isVisible();};
-                }
-
-                //PHONE
-                if(scope.question.type === QuestionService.getTypes().phone){
-                    var onChange = scope.question.onChange ? scope.question.onChange : function(){};
-                    scope.question.onChange = function(question){
-                        if(question.model[question.id].countryCode.code === 'SWE') {
-                            if (question.model[question.id].phoneNumber === '') {
-                                question.model[question.id].phoneNumber = '0';
-                            }
-
-                            if(question.getAnswer().phoneNumber.toString().length >= 2 && question.getAnswer().phoneNumber.toString().substr(0,2) === '00'){
-                                question.model[question.id].phoneNumber = '0'+question.getAnswer().phoneNumber.toString().substr(2);
-                            }
-                        }
-                        onChange(question);
-                    };
-                    scope.addZeroToPhonenumber = function(){
-                        if(scope.question.model[scope.question.id].phoneNumber === '0' && scope.question.model[scope.question.id].countryCode.code !== 'SWE'){
-                            scope.question.model[scope.question.id].phoneNumber = '';
-                        } else if(scope.question.model[scope.question.id].phoneNumber === '' && scope.question.model[scope.question.id].countryCode.code === 'SWE'){
-                            scope.question.model[scope.question.id].phoneNumber = '0';
-                        }
-                    };
-                }
-            }
-        };
-    }]);
-
-'use strict';
-angular.module('fsmQuestion').filter('fsmTranslate', ['$translate', function($translate){
-    function fsmTranslate(key){
-        var translation = $translate.instant(key);
-        if(translation === key){
-            return '';
-        }
-        return translation;
-    }
-
-    return fsmTranslate;
-}]);
 angular.module('templates-app', ['buttongroup.tpl.html', 'buttongroupbig.tpl.html', 'checkbox.tpl.html', 'date.tpl.html', 'datetime.tpl.html', 'formerror.tpl.html', 'input.tpl.html', 'inputcurrency.tpl.html', 'location.tpl.html', 'phone.tpl.html', 'question.tpl.html', 'select.tpl.html', 'text.tpl.html', 'tooltip.tpl.html', 'upload.tpl.html']);
 
 angular.module("buttongroup.tpl.html", []).run(["$templateCache", function($templateCache) {
