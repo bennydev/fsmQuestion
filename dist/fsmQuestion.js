39ccb424dbdad3abd65ea339c312cb991432805d
Johnny Nilsson <Johnny.Nilsson@folksam.se>
2016-01-08 17:26:57 +0100

    updated the input-template to use translate-values

--- dist/fsmQuestion.js
+++ dist/fsmQuestion.js
@@ -190,7 +190,7 @@ angular.module("templates/fsmQuestionGroup.tpl.html", []).run(["$templateCache",
 angular.module("templates/input.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("templates/input.tpl.html",
     "<div class=\"form-label\">\n" +
-    "    <label for=\"{{question.id}}\">{{question.text.root+'.QUESTION' | translate}}<span ng-show=\"question.isRequired()\" class=\"required\"></span></label>\n" +
+    "    <label for=\"{{question.id}}\"><span translate translate-default=\" \" translate-values=\"question.text.getTranslateValues()\">{{question.text.root+'.QUESTION'}}</span><span ng-show=\"question.isRequired()\" class=\"required\"></span></label>\n" +
     "</div>\n" +
     "<div class=\"grid\">\n" +
     "    <div class=\"grid__item sm--six-twelfths\">\n" +
