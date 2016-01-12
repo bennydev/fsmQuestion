9a7dc0eb053e012ec1b0c7f165fe78aa64e263a9
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-12 11:02:02 +0100

    Added  unittest functionality and changed text.tpl.html to always show a zero count for the answer (if undefined).

---
+++ src/app.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Sanity check test", function() {
+    var ctrl;
+
+    beforeEach(module('fsmQuestion'));
+    ctrl = module('fsmQuestion');
+
+    it('should have a fsmQuestion module', function () {
+        expect(ctrl).toBeDefined();
+    });
+});
