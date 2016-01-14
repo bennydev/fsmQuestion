c93e02ad3faa8535c5e5d10bca0318b6e1fadfa4
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-14 13:24:03 +0100

    Fixed max validtion for dates, build.

--- src/validators.js
+++ src/validators.js
@@ -40,7 +40,7 @@ function Validators(QuestionTypes){
             validate: function(question){
                 var answer = question.answer;
                 var result = {};
-                result.valid = dateInMillis(answer) <= question.restrictions.date.getMax().getTime();
+                result.valid = dateInMillis(answer) <= question.restrictions.getMax().date.getTime();
                 result.message = question.text.root + '.ERRORS.TOO_LATE';
                 return result;
             }
