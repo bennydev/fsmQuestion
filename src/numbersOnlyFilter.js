bf59e2d42aff55a2b618e53ea1fa4d11903fd436
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-15 10:43:42 +0100

    Added comment.

--- src/numbersOnlyFilter.js
+++ src/numbersOnlyFilter.js
@@ -2,6 +2,8 @@
 angular.module('fsmQuestion').filter('numbersOnly', function () {
     return function(input) {
         if (input) {
+
+            // Remove trailing zeros
             var str = input.replace(/^0/, '');
             str = str.replace(/[^0-9]+/g, '');
             return str;
