82021734a40551f19719880009e70a79ec59cbf5
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-15 10:26:12 +0100

    Removed debug logging.

--- src/numbersOnlyFilter.js
+++ src/numbersOnlyFilter.js
@@ -2,11 +2,8 @@
 angular.module('fsmQuestion').filter('numbersOnly', function () {
     return function(input) {
         if (input) {
-            console.log('Input: ' + input);
             var str = input.replace(/^0/, '');
-            console.log('After first: ' + str);
             str = str.replace(/[^0-9]+/g, '');
-            console.log('Completed: ' + str);
             return str;
         }
 
