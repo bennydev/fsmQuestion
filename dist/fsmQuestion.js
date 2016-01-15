6d16d062675215bbdbd1168193fb3cbd0cd2ac28
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-15 10:55:39 +0100

    Build.

--- dist/fsmQuestion.js
+++ dist/fsmQuestion.js
@@ -233,11 +233,10 @@ angular.module('fsmQuestion')
 angular.module('fsmQuestion').filter('numbersOnly', function () {
     return function(input) {
         if (input) {
-            console.log('Input: ' + input);
+
+            // Remove trailing zeros
             var str = input.replace(/^0/, '');
-            console.log('After first: ' + str);
             str = str.replace(/[^0-9]+/g, '');
-            console.log('Completed: ' + str);
             return str;
         }
 
