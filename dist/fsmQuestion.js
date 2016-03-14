8dff4158fe5a524e310b0f550c02679c64d67085
bennydev <benny.henriksson@cag.se>
2016-03-14 12:28:55 +0100

    Fixed bug in questionStorage.questionsHasLocalStorage.

--- dist/fsmQuestion.js
+++ dist/fsmQuestion.js
@@ -555,123 +555,126 @@ function QuestionService(QuestionBuilder, Question, Options, Restrictions, Valid
     }
 }
 
-"use strict";
-angular.module('fsmQuestion')
-    .factory('QuestionStorage', ['storagePrefix', 'localStorageService', QuestionStorage]);
-function QuestionStorage(storagePrefix, localStorageService) {
-    var questions = {};
-    var questionLocalStorageDone = false;
-    var loadStoredAnswers = false;
-    var localStore = {};
-    var service = {
-        contains: contains,
-        addQuestion: addQuestion,
-        getQuestion: getQuestion,
-        loadAnswer: loadAnswer,
-        loadObject: loadObject,
-        saveAnswer: saveAnswer,
-        reload: reload,
-        questionsHasLocalStorage: questionsHasLocalStorage,
-        clear: clear
-    };
-    loadLocalStore();
-    return service;
-    function contains(id) {
-        return !!questions[id];
-    }
-
-    function loadLocalStore() {
-        var keys = localStorageService.keys();
-        if (keys) {
-            keys.forEach(function (key) {
-                localStore[key] = localStorageService.get(key);
-            });
+(function() {
+    "use strict";
+    angular.module('fsmQuestion')
+        .factory('QuestionStorage', QuestionStorage);
+    QuestionStorage.$inject = ['storagePrefix', 'localStorageService'];
+    function QuestionStorage(storagePrefix, localStorageService) {
+        var questions = {};
+        var questionLocalStorageDone = false;
+        var loadStoredAnswers = false;
+        var localStore = {};
+        var service = {
+            contains: contains,
+            addQuestion: addQuestion,
+            getQuestion: getQuestion,
+            loadAnswer: loadAnswer,
+            loadObject: loadObject,
+            saveAnswer: saveAnswer,
+            reload: reload,
+            questionsHasLocalStorage: questionsHasLocalStorage,
+            clear: clear
+        };
+        loadLocalStore();
+        return service;
+        function contains(id) {
+            return !!questions[id];
         }
-    }
 
-    function addQuestion(question) {
-        questions[question.id] = question;
-    }
+        function loadLocalStore() {
+            var keys = localStorageService.keys();
+            if (keys) {
+                keys.forEach(function (key) {
+                    localStore[key] = localStorageService.get(key);
+                });
+            }
+        }
 
-    function getQuestion(id) {
-        return questions[id];
-    }
+        function addQuestion(question) {
+            questions[question.id] = question;
+        }
 
-    function getStorageKey(id) {
-        return storagePrefix + '.questions.' + id;
-    }
+        function getQuestion(id) {
+            return questions[id];
+        }
 
-    function loadAnswer(id) {
-        if (loadStoredAnswers) {
+        function getStorageKey(id) {
+            return storagePrefix + '.questions.' + id;
+        }
+
+        function loadAnswer(id) {
+            if (loadStoredAnswers) {
+                return localStore[getStorageKey(id)];
+            }
+            return undefined;
+        }
+
+        /**
+         * Load objects that does not belong to a Question's answer.
+         * @param id
+         * @returns {*}
+         */
+        function loadObject(id) {
             return localStore[getStorageKey(id)];
         }
-        return undefined;
-    }
 
-    /**
-     * Load objects that does not belong to a Question's answer.
-     * @param id
-     * @returns {*}
-     */
-    function loadObject(id) {
-        return localStore[getStorageKey(id)];
-    }
-
-    function questionsHasLocalStorage(questions) {
-        if (!questionLocalStorageDone) {
-            questionLocalStorageDone = true;
-            for (var i = 0; i < questions.length; i++) {
-                if (!compareValues(questions[i].answer), localStore[getStorageKey(questions[i].id)]) {
-                    return false;
+        function questionsHasLocalStorage(questions) {
+            if (!questionLocalStorageDone) {
+                questionLocalStorageDone = true;
+                for (var i = 0; i < questions.length; i++) {
+                    if (!compareValues(questions[i].answer, localStore[getStorageKey(questions[i].id)])) {
+                        return false;
+                    }
                 }
+                return true;
             }
-            return true;
+            return false;
         }
-        return false;
-    }
 
-    function compareValues(answer, storedValue) {
-        if (!answer && !storedValue) {
-            return true;
+        function compareValues(answer, storedValue) {
+            if (!answer && !storedValue) {
+                return true;
+            }
+            return answer === storedValue;
         }
-        return answer === storedValue;
-    }
 
-    function saveAnswer(id, answer) {
-        if (isNotNull(answer)) {
-            localStorageService.set(getStorageKey(id), answer);
-        }
-    }
-
-    function reload() {
-        loadStoredAnswers = true;
-        Object.keys(questions).forEach(function (id) {
-            var question = getQuestion(id);
-            var answer = loadAnswer(id);
-            //console.log(id + '===' + answer);
+        function saveAnswer(id, answer) {
             if (isNotNull(answer)) {
-                question.answer = answer;
-                if (question.options.onChange) {
-                    question.options.onChange(question);
-                }
+                localStorageService.set(getStorageKey(id), answer);
             }
+        }
 
-        });
-    }
+        function reload() {
+            loadStoredAnswers = true;
+            Object.keys(questions).forEach(function (id) {
+                var question = getQuestion(id);
+                var answer = loadAnswer(id);
+                //console.log(id + '===' + answer);
+                if (isNotNull(answer)) {
+                    question.answer = answer;
+                    if (question.options.onChange) {
+                        question.options.onChange(question);
+                    }
+                }
 
-    function isNotNull(value) {
-        return value !== undefined && value !== null && value !== '';
-    }
+            });
+        }
 
-    function clear() {
-        // This does not work for some reason, should investigate.
-        //Object.keys(questions).forEach(function(id) {
-        //    localStorageService.remove(getStorageKey(id));
-        //});
-        localStore = {};
-        localStorageService.clearAll();
+        function isNotNull(value) {
+            return value !== undefined && value !== null && value !== '';
+        }
+
+        function clear() {
+            // This does not work for some reason, should investigate.
+            //Object.keys(questions).forEach(function(id) {
+            //    localStorageService.remove(getStorageKey(id));
+            //});
+            localStore = {};
+            localStorageService.clearAll();
+        }
     }
-}
+})();
 "use strict";
 angular.module('fsmQuestion')
 .value('QuestionTypes', new QuestionTypes());
